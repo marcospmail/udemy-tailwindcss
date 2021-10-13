064220eca58606a4b0acd896489d6d9f430e2cd0
Marcos Paulo <marcosp.mail@gmail.com>
2021-10-12 23:16:56 -0300

    Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     debugScreens: {
       position: ['top', 'left']
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        headline: ['Oswald']
+      },
+      colors: {
+        mainColor: "#212f49"
+      }
+    },
   },
   variants: {
     extend: {},
